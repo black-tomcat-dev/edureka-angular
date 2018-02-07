a4191fa077c49ec8620a1eb1027f1707a7986fa4
black-tomcat-dev <kevin.vdm@protonmail.com>
2018-02-07 09:11:38 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import {ButtonsModule} from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { AboutComponent } from './components/about/about.component';
+
+const appRoutes: Routes = [
+  {path:'', component:UserComponent},
+  {path:'about', component:AboutComponent},
 
 
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    ButtonsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
